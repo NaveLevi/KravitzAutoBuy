35fb48cdd59ecdd9e28c799a52fc95c4aef05d45
Nave Levi <navelevi@outlook.com>
2020-09-28 12:48:24 +0300

    added retries, code cleanup and general improvments

--- main.py
+++ main.py
@@ -23,18 +23,23 @@ def telegramNotify(msg):
    print(msgUrl)
    return True
 
-def waitAndClick(element, sleep=0):
+def waitAndClick(element,driver, sleep=0):
    time.sleep(sleep)
    for x in range(150):
-      print(x)
       try:
+         print("Looking for {} for the {} time".format(element, x))
          driver.find_element_by_xpath(element).click()
       except:
          time.sleep (0.1)
+         if x == 100: #if tried 100 times and didn't find the button, refresh the page
+            driver.refresh()
+            time.sleep(5)
          continue
       print("clicking! "+element )
       return True
+   exit()
    return False
+
 
 
 def sendImage(img,userId,token):
@@ -42,49 +47,72 @@ def sendImage(img,userId,token):
     files = {'photo': open(img , 'rb')}
     data = {'chat_id' : userId}
     r= requests.post(url, files=files, data=data)
-    print('Imeg sent')
+    print('Image sent')
 
-#----------------------------------------------- MAIN
-while True: #todo: move everything to try
-   driver = webdriver.Chrome(chromeDriverPath)
+def Main(driver):
    driver.get(productUrl)
-
-   waitAndClick('//*[@id="product-addtocart-button"]', 5)
+   waitAndClick('//*[@id="product-addtocart-button"]', driver, 5)
    telegramNotify("I've added a product to the cart!")
    driver.get("https://www.kravitz.co.il/edea/cart/")
    #todo: add check that cart has 1 item
-   waitAndClick('//*[@id="cart-summary"]/div[1]/ul/li/button')
+   # time.sleep(5)
+   # print("trying to get text from box")
+   # text = driver.find_element_by_xpath('/html/body/div[3]/main/div/div[2]/div/div[2]/div[2]/div/div/div/div[2]/div/div[2]/div[4]/div/div/input').getText("my text")
+   # print("The text I got is:{}".format(text))
+
+   waitAndClick('//*[@id="cart-summary"]/div[1]/ul/li/button', driver)
    time.sleep(5)
+   waitAndClick('//*[@id="checkout-shipping-method-load"]/div/div/input', driver)
    driver.find_element_by_xpath('//*[@id="customer-email"]').send_keys(data['email'])
-   
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/div[1]/div/input').send_keys(data['name'])
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/div[3]/div/input').send_keys(data['city'])
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/fieldset/div/div[1]/div/input').send_keys(data['street'])
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/fieldset/div/div[2]/div/input').send_keys(data['homeNumber'])
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/fieldset/div/div[3]/div/input').send_keys(data['aptNumber'])
    driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[1]/div[2]/form[2]/div/div[7]/div/input').send_keys(data['phoneNumber'])
-   waitAndClick('//*[@id="checkout-shipping-method-load"]/div/div/input')
    # driver.find_elements_by_xpath
 
    #payment
-   waitAndClick('html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[2]/div/div[3]/form/div[3]/div/button')
+   waitAndClick('html/body/div[2]/main/div/div/div[2]/div[2]/div[4]/ol/li[2]/div/div[3]/form/div[3]/div/button', driver)
    time.sleep(15)
    driver.switch_to.frame(0)
    print("switched frames!!!")
-   waitAndClick('//*[@id="date_month_input"]/option[{}]'.format(data['expiryMonth']))
-   waitAndClick('//*[@id="date_year_input"]/option[{}]'.format(data['expiryYear'])) #2019 is option1, going up. = year - 2018
+   waitAndClick('//*[@id="date_month_input"]/option[{}]'.format(data['expiryMonth']), driver)
+   waitAndClick('//*[@id="date_year_input"]/option[{}]'.format(data['expiryYear']), driver) #2019 is option1, going up. = year - 2018
    driver.find_element_by_xpath('//*[@id="id_number_input"]').send_keys(data['taz'])
    driver.find_element_by_xpath('//*[@id="credit_card_number_input"]').send_keys(data['creditCard'])
    driver.find_element_by_xpath('//*[@id="cvv_input"]').send_keys(data['ccv'])
    driver.find_element_by_xpath('//*[@id="submitBtn"]').click()
-
-   time.sleep(15) # sleep in order to acount for purchesing time before screenshot 
+   
+   with open('status.txt', 'w') as statusFile:
+      statusFile.write('done')
    currentTime = time.strftime("%H.%M.%S", time.localtime())
    imgPath = currentTime+".png"
+   time.sleep(15) # sleep in order to acount for purchesing time before screenshot
    driver.save_screenshot(imgPath)
    print('Taking screenshot')
-
    sendImage(imgPath,data['telegramIdToNotify'],data['telegramToken'])
-   time.sleep(100)
-   exit()
-   
+   return True    
+
+
+#----------------------------------------------- MAIN
+tryCounter=1
+# driver = webdriver.Chrome(chromeDriverPath)
+while True:
+   print("Trying to run the script for  the {}nd time".format(tryCounter))
+   with open('status.txt') as f:
+      statusLine = f.readline()
+   if statusLine != 'done':
+      try:
+         driver = webdriver.Chrome(chromeDriverPath)
+         Main(driver)
+         print("Done! Hopefully you got the product you wanted")
+      except:
+         driver.quit()
+         tryCounter+=1
+         continue
+      exit(0)
+   else:
+      print("You already got the product! Exiting")
+      exit(0)
+         
